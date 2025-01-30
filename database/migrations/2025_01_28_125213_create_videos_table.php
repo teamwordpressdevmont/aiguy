a61032c79470544457e04b5b8f0bb4412125157d
iamzainzai <iamzainzai@gmail.com>
2025-01-30 18:10:09 +0500

    category_id

--- database/migrations/2025_01_28_125213_create_videos_table.php
+++ database/migrations/2025_01_28_125213_create_videos_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('imageurl')->nullable();
             $table->text('description');
-            $table->string('category_id');
+            $table->string('category_id')->nullable();
             $table->timestamps();
         });
     }
