a454608aa98ca00b60012933e5ef898fba3884a5
iamzainzai <iamzainzai@gmail.com>
2025-01-31 11:53:23 +0500

    change api

--- routes/api.php
+++ routes/api.php
@@ -29,8 +29,8 @@ Route::prefix('courses')->group(function () {
 
 Route::prefix('bookmark')->group(function() {
     Route::post('/folder', [BookmarkController::class, 'createBookmarkFolder']); // Create Folder
-    Route::post('/folder/{folderId}/tool', [BookmarkController::class, 'addToolToFolder']); // Add Tool
     Route::post('/folder/{folderId}', [BookmarkController::class, 'updateBookmarkFolder']); //Update Folder
+    Route::post('/folder/{folderId}/tool', [BookmarkController::class, 'addToolToFolder']); // Add Tool
     Route::delete('/folder/{folderId}/tool/{toolId}', [BookmarkController::class, 'removeToolFromFolder']); // Remove Tool
     Route::get('/folders', [BookmarkController::class, 'listFolders']); // List Folders
     Route::get('/folder/{folderId}/share', [BookmarkController::class, 'shareFolder']); // Share Folder
