f3fddb3d5d51c1d3a4e468d166ac862761527deb
shahzaddevmont <shahzaddevmont@gmail.com>
2025-02-07 11:46:07 +0500

    Blog Add updated

--- resources/views/blog/blog.blade.php
+++ resources/views/blog/blog.blade.php
@@ -3,11 +3,18 @@
 
 <div class="container mx-auto">
 
+
+
+
+
 <form>
   <div class="space-y-12">
-    <div class="border-b border-gray-900/10 pb-12">
+    <div class="pb-12">
 
-      <div class="mt-10  col-span-full">
+      
+
+      <div class="mt-10 col-span-full">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900 mb-10">Add Blog</h1>
         <div class="sm:col-span-4 mb-5">
           <label for="username" class="block text-sm/6 font-medium text-gray-900">Blog Title</label>
           <div class="mt-2">
@@ -23,7 +30,6 @@
           <div class="mt-2">
             <textarea name="about" id="about" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
           </div>
-          <p class="mt-3 text-sm/6 text-gray-600">Write a few sentences about yourself.</p>
         </div>
 
         <div class="col-span-full mb-5">
@@ -65,8 +71,8 @@
 
   </div>
 
-  <div class="mt-6 flex items-center justify-end gap-x-6">
-    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+  <div class="flex items-center justify-end gap-x-6 pb-10">
+    <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Publish</button>
   </div>
 </form>
 
