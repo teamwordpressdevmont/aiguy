b02f2681309ee8a6f833d2804d0986bf2fbad746
shahzaddevmont <shahzaddevmont@gmail.com>
2025-02-07 13:05:27 +0500

    index and blog updated

--- resources/views/blog/blog.blade.php
+++ resources/views/blog/blog.blade.php
@@ -132,7 +132,6 @@
               <label for="username" class="block text-sm/6 font-medium text-gray-900">Title</label>
               <div class="mt-2">
                 <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                
                   <input type="text" name="username" id="username" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="janesmith">
                 </div>
               </div>
@@ -182,7 +181,6 @@
 
         <div class="flex items-center justify-end gap-x-6">
           <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Publish</button>
-
         </div>
 
       </form>    
