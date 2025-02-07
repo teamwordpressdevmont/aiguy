3497adcb616c4e3860804fa4d71bb1ec321ffbd9
shahzaddevmont <shahzaddevmont@gmail.com>
2025-02-07 19:56:04 +0500

    AI Tool Single Updated

--- resources/views/ai-tools/view.blade.php
+++ resources/views/ai-tools/view.blade.php
@@ -5,8 +5,6 @@
 
 
 
-    
-
 
     <div class="mt-5 mx-auto max-w-7xl bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 mx-auto">
         <img class="rounded-t-lg w-full h-100 object-cover object-center -mb-12" src="{{ asset('storage/' . $tool->cover) }}" alt="" />
@@ -18,3 +16,9 @@
             </div>
         </div>
     </div>
+    <div class="mx-auto max-w-7xl py-6">
+    <a href="{{ route('ai-tools.list') }}" class=" rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Back To List
+    </a> 
+    </div>    
+
