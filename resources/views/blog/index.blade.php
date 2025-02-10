acc6e9d7895ba58b731d0bb87c3780dc9040b879
Umer Hussain <umerhussain.webdeveloper@gmail.com>
2025-02-10 18:13:35 +0500

    updated.

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -269,7 +269,6 @@
                             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                                 <input type="text" name="sub_title" id="sub_title" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="janesmith" value="{{ old('sub_title', $blog->sub_title ?? '') }}">
                             </div>
-                        @endif
                     </div>
                     <div class="col-span-full mb-5">
                         <label class="block text-sm font-medium">Left Image</label>
