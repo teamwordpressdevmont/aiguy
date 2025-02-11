24a80a98e9905fe0b672f94ed80a6d8e4d1dee98
Umer Hussain <umerhussain.webdeveloper@gmail.com>
2025-02-11 11:08:31 +0500

    updated.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,7 +6,7 @@
 @if($currentController == 'AiToolDataController' || $currentController == 'AiToolCategoryController')
     <div class="">
 
-    <div class="min-h-full">
+    <div class="">
         <nav class="bg-gray-800">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div class="flex h-16 items-center justify-between">
